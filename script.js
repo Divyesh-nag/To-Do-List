04a8f0af14456d20d6ee871575e4b7388d29f53a
Divyesh Nag <79678694+Divyesh-nag@users.noreply.github.com>
2021-05-30 09:56:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let addToDoButton = document.getElementById('addToDo');
+let todocontainer = document.getElementById('todocontainer');
+let inputfield = document.getElementById('inputfield');
+
+addToDoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputfield.value;
+    todocontainer.appendChild(paragraph);
+    inputfield.value = "";
+    paragraph.addEventListener('dblclick', function(){
+        todocontainer.removeChild(paragraph);
+    })
+})
